52f23399d2649ff089272d5f51deae755faa9de3
lewyziomal <tomaszlewandowski073@gmail.com>
2025-06-19 14:07:40 +0200

    whip: prepared add_user

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from utils.model import users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 
 
 def main():
     print('===========MENU============')
     print('0 - zakończ program ')
     print('1 - wyswietl co u znajomych ')
+    print('2 - dodaj noego uzytkownika')
+    print('3 - usun uzytkownika  ')
+    print('4 - edytuj uzytkownika ')
+    print('===========MENU============')
 
     while True:
         choice:str=input("Wybierz opcje MENU: ")
-        if choice == '0': break
-        if choice == '1': get_user_info(users)
+        if choice == '0':
+            break
+        if choice == '1':
+            get_user_info(users)
+        if choice == '2':
+            add_user(users)
 
     get_user_info(users)
 
--- utils/controller.py
+++ utils/controller.py
@@ -1,3 +1,10 @@
-def get_user_info(user_data:list)->None:
+def get_user_info(user_data: list) -> None:
     for user in user_data:
-        print(f"Twój znajomy {user['name']}! z mieiscowosci {user['location']} opublikował {user[ 'posts']} postów")
+        print(f"Twój znajomy {user['name']}! z mieiscowosci {user['location']} opublikował {user['posts']} postów")
+
+
+def add_user(user_data: list) -> None:
+    new_name = input('podaj imie nowego uzytkownika: ')
+    new_location = input('podaj Lokalizacje nowego użytkownika: ')
+    new_posts = int(input('podai Liczbe postow nowego użytkownika: '))
+    user_data.append({"name": new_name, "location": new_location, "posts": new_posts})
