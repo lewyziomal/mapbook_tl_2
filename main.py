531d65f597e86e55bc3ba0d5f1b3d8526772b7e0
lewyziomal <tomaszlewandowski073@gmail.com>
2025-06-19 22:46:28 +0200

    whip:  gui+mapa

--- main.py
+++ main.py
@@ -33,9 +33,10 @@
 #     main()
 
 
-
+from cProfile import Profile
 from tkinter import *
 
+import tkintermapview
 
 root = Tk()
 
@@ -45,65 +46,94 @@ root.geometry('1200x700')
 ramka_lista_obiektow = Frame(root)
 ramka_formularz = Frame(root)
 ramka_szcegoly_obiektow = Frame(root)
+ramka_mapa = Frame(root)
 
 ramka_lista_obiektow.grid(row=0, column=0, padx=50)
 ramka_formularz.grid(row=0, column=1)
-ramka_szcegoly_obiektow.grid(row=1, column=0)
+ramka_szcegoly_obiektow.grid(row=1, column=0, columnspan=2)
+ramka_mapa.grid(row=2, column=0, columnspan=2)
 
 # ramka_lista_obiektow
 
 label_lista_obiektow = Label(ramka_lista_obiektow, text='Lista Znajomych')
-label_lista_obiektow.grid(row=0, column=0,)
+label_lista_obiektow.grid(row=0, column=0, )
 
-listbox_lista_obiektow=Listbox(ramka_lista_obiektow)
+listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
 
-button_pokaz_szczegoly=Button(ramka_lista_obiektow, text='Pokaż szczególy')
+button_pokaz_szczegoly = Button(ramka_lista_obiektow, text='Pokaż szczególy')
 button_pokaz_szczegoly.grid(row=3, column=0)
 
-button_usun_obiekt=Button(ramka_lista_obiektow, text='Usuń znajomego')
+button_usun_obiekt = Button(ramka_lista_obiektow, text='Usuń znajomego')
 button_usun_obiekt.grid(row=3, column=1)
 
-button_edytuj_obiekt=Button(ramka_lista_obiektow, text='Edytuj obiekt')
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text='Edytuj obiekt')
 button_edytuj_obiekt.grid(row=3, column=2)
 
 # ramka_formularz
 
-label_formularz=Label(ramka_formularz, text='Formularz')
+label_formularz = Label(ramka_formularz, text='Formularz')
 label_formularz.grid(row=0, column=1, columnspan=3)
 
-label_imie=Label(ramka_formularz, text='Imie')
+label_imie = Label(ramka_formularz, text='Imie')
 label_imie.grid(row=2, column=0, sticky=W)
 
-label_nazwisko=Label(ramka_formularz, text='Nazwisko')
-label_nazwisko.grid(row=3, column=0,sticky=W)
+label_nazwisko = Label(ramka_formularz, text='Nazwisko')
+label_nazwisko.grid(row=3, column=0, sticky=W)
 
-label_miejscowosc=Label(ramka_formularz, text='Miejscowosc')
-label_miejscowosc.grid(row=4, column=0,sticky=W)
+label_miejscowosc = Label(ramka_formularz, text='Miejscowosc')
+label_miejscowosc.grid(row=4, column=0, sticky=W)
 
-label_liczba_postow=Label(ramka_formularz, text='Liczba postow')
-label_liczba_postow.grid(row=5, column=0,sticky=W)
+label_liczba_postow = Label(ramka_formularz, text='Liczba postow')
+label_liczba_postow.grid(row=5, column=0, sticky=W)
 
-entry_imie=Entry(ramka_formularz)
+entry_imie = Entry(ramka_formularz)
 entry_imie.grid(row=2, column=1)
-entry_nazwisko=Entry(ramka_formularz)
+entry_nazwisko = Entry(ramka_formularz)
 entry_nazwisko.grid(row=3, column=1)
-entry_miejscowosc=Entry(ramka_formularz)
+entry_miejscowosc = Entry(ramka_formularz)
 entry_miejscowosc.grid(row=4, column=1)
-entry_liczba_postow=Entry(ramka_formularz)
+entry_liczba_postow = Entry(ramka_formularz)
 entry_liczba_postow.grid(row=5, column=1)
 
-button_dodaj_uzytkownika=Button(ramka_formularz, text='Dodaj uzytkownika')
+button_dodaj_uzytkownika = Button(ramka_formularz, text='Dodaj uzytkownika')
 button_dodaj_uzytkownika.grid(row=6, column=1, columnspan=2)
 
 # ramka_szczegoly_obiektow
 
-label_szczegoly_uzytkownika=Label(ramka_szcegoly_obiektow, text='Pokaż szczegóły:')
+label_szczegoly_uzytkownika = Label(ramka_szcegoly_obiektow, text='Pokaż szczegóły:')
 label_szczegoly_uzytkownika.grid(row=0, column=0)
 
-label_imie_szczegoly_uzytkwnika=Label(ramka_szcegoly_obiektow, text='Imie: ')
+label_imie_szczegoly_uzytkwnika = Label(ramka_szcegoly_obiektow, text='Imie: ')
 label_imie_szczegoly_uzytkwnika.grid(row=1, column=0)
 
+label_imie_szczegoly_uzytkwnika_wartosc = Label(ramka_szcegoly_obiektow, text='........ ')
+label_imie_szczegoly_uzytkwnika_wartosc.grid(row=1, column=1)
+
+label_nazwisko_szczegoly_uzytkwnika = Label(ramka_szcegoly_obiektow, text='Nazwisko: ')
+label_nazwisko_szczegoly_uzytkwnika.grid(row=1, column=2)
+
+label_nazwisko_szczegoly_uzytkwnika_wartosc = Label(ramka_szcegoly_obiektow, text='........ ')
+label_nazwisko_szczegoly_uzytkwnika_wartosc.grid(row=1, column=3)
+
+label_miejscowosc_szczegoly_uzytkwnika = Label(ramka_szcegoly_obiektow, text='Miejscowosc: ')
+label_miejscowosc_szczegoly_uzytkwnika.grid(row=1, column=4)
+
+label_miejscowosc_szczegoly_uzytkwnika_wartosc = Label(ramka_szcegoly_obiektow, text='........ ')
+label_miejscowosc_szczegoly_uzytkwnika_wartosc.grid(row=1, column=5)
+
+label_liczba_postow_szczegoly_uzytkwnika = Label(ramka_szcegoly_obiektow, text='Liczba postów: ')
+label_liczba_postow_szczegoly_uzytkwnika.grid(row=1, column=6)
+
+label_liczba_postow_szczegoly_uzytkwnika_wartosc = Label(ramka_szcegoly_obiektow, text='........ ')
+label_liczba_postow_szczegoly_uzytkwnika_wartosc.grid(row=1, column=7)
+
+# ramka_mapa
+
+map_widget=tkintermapview.TkinterMapView(ramka_mapa, width=1200, height=470)
+map_widget.grid(row=0, column=0, columnspan=2)
+map_widget.set_position(52.23, 21.00)
+map_widget.set_zoom(6)
 
 
 
